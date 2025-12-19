ab7f8921614d070983408ec412f28e9950c6889c
jaimetur <jaimetur@gmail.com>
2025-12-19 03:24:32 +0100

    -v0.4.3

--- src/RetuningAutomations.py
+++ src/RetuningAutomations.py
@@ -43,7 +43,7 @@ from src.modules.CleanUp.FinalCleanUp import FinalCleanUp
 # ================================ VERSIONING ================================ #
 
 TOOL_NAME           = "RetuningAutomations"
-TOOL_VERSION        = "0.4.2"
+TOOL_VERSION        = "0.4.3"
 TOOL_DATE           = "2025-12-19"
 TOOL_NAME_VERSION   = f"{TOOL_NAME}_v{TOOL_VERSION}"
 COPYRIGHT_TEXT      = "(c) 2025 - Jaime Tur (jaime.tur@ericsson.com)"
